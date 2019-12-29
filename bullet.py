ef3f35e83029088b8a76d39dd3674512e68f4bf5
JimmyWu-812 <59006023+JimmyWu-812@users.noreply.github.com>
2019-12-29 16:00:15 +0800

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -4,7 +4,7 @@ class bullet:
     name
     cd:countdown time
     nop:number of projectiles
-    dop:damage of one projectile
+    damage:damage of one projectile
     rop:radius of projectiles
     rod:radius of damage
     ang:spreading angle
@@ -23,7 +23,7 @@ class bullet:
     bwizard=bullet.bullet('Wizard',2.2,1,1000,140,50,0,None,'salmon','wizard.gif')
     bice_wizard=bullet.bullet('Ice Wizard',2.2,1,400,110,20,0,'Freeze','azure','snowball.gif')
     '''
-    def __init__(self,name,cd,nop,dop,rop,rod,ang,eff,traj_col,bul_gif,attack_ratio):
+    def __init__(self,name,cd,nop,damage,rop,rod,ang,eff,traj_col,bul_gif,attack_ratio):
         if nop==1:
             self.items=turtle.Turtle()
             if name=='Electro Wizard' or name=='Sparky':
@@ -44,7 +44,7 @@ class bullet:
         self.name=name
         self.cd=cd
         self.nop=nop
-        self.dop=dop
+        self.damage=damage
         self.rop=rop
         self.rod=rod
         self.ang=ang
