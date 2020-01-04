c3f2f6360cc2649f569b04debfb987c0ecdb4306
joshuanieh <b07502027@ntu.edu.tw>
2020-01-04 12:35:36 +0800

    player figure

--- main.py
+++ main.py
@@ -31,6 +31,9 @@ for i in range(4):
         screen.addshape(f'player1_frozen-{90*i}.gif')
         screen.addshape(f'player2_frozen-{90*i}.gif')
         screen.addshape(f'player_electrified-{90*i}.gif')
+        
+screen.addshape('player1.gif')
+screen.addshape('player2.gif')
 screen.addshape('ATK+.gif')
 screen.addshape('DEF+.gif')
 screen.addshape('HEAL.gif')
